e3f116b3974adf19975344b22ed53b942b04bdcd
Sage Lupi <sagelupi@Sages-MacBook-Pro.local>
2023-06-28 19:43:16 -0400

    Added questions

--- Develop/utils/index.js
+++ Develop/utils/index.js
@@ -1,10 +1,38 @@
-// TODO: Include packages needed for this application
+// TODO: Include packages needed for this application 
+// Added in package-lock.json
+
+const inquirer = require('inquirer');
+const fs = require('fs');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+{
+    type: 'input',
+    name: 'title',
+    message: 'What is the title of your project?'
+},
+{
+    type: 'input',
+    name: 'description',
+    message: 'Please provide a brief description of your project.'
+},
+{
+    type: 'input',
+    name: 'instructions',
+    message: 'Please explain how to use your website.'
+}
+];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName, data, (err) => {
+        if(err) {
+            console.error(err);
+        } else {
+            console.log(`Successfully generated ${filename}!`);
+        }
+    })
+}
 
 // TODO: Create a function to initialize app
 function init() {}
