fb208e20fecf4c82fa5090a4bfd62448248e16ef
Sage Lupi <sagelupi@Sages-MacBook-Pro.local>
2023-06-28 21:26:11 -0400

    Added in content into index.js

--- Develop/utils/index.js
+++ Develop/utils/index.js
@@ -1,66 +1,59 @@
+const inquirer = require('inquirer').default;
+const fs = require('fs');
+
 // TODO: Create an array of questions for user input
-const inquirer = require('inquirer');
 const questions = [
-    // your questions here
-  ];
-  
-  // TODO: Create a function to write README file
-  function writeToFile(fileName, data) {
-    // your writeToFile implementation
-  }
-  
-  // TODO: Create a function to initialize app
-  async function init() {
-    const { default: inquirer } = await import('inquirer');
-    // rest of your code
-  }
-  
-  // Function call to initialize app
-  init();
-  
-  
-  // Function call to initialize app
-  init();
-  
-
-
-// TODO: Include packages needed for this application 
-// Added in package-lock.json
-//const inquirer = require('inquirer');
-//const fs = require('fs');
-
-// TODO: Create an array of questions for user input
-//const questions = [
-// {
-    //  type: 'input',
-    //  name: 'title',
-    //  message: 'What is the title of your project?'
-//},
-//{
-    //  type: 'input',
-    //  name: 'description',
-    //  message: 'Please provide a brief description of your project.'
-//},
-//{
-    //  type: 'input',
-    //  name: 'instructions',
-    //  message: 'Please explain how to use your website.'
-//}
-//];
+  {
+    type: 'input',
+    name: 'title',
+    message: 'What is the title of your project?',
+  },
+  {
+    type: 'input',
+    name: 'description',
+    message: 'Please provide a brief description of your project:',
+  },
+  {
+    type: 'input',
+    name: 'instructions',
+    message: 'Please explain how to use your website:',
+  },
+];
 
 // TODO: Create a function to write README file
-//function writeToFile(fileName, data) {
-    //fs.writeFile(fileName, data, (err) => {
-        //if(err) {
-         //   console.error(err);
-        //} else {
-        //  console.log(`Successfully generated ${filename}!`);
-        //}
-    //})
-//}
+function writeToFile(fileName, data) {
+  fs.writeFile(fileName, data, (err) => {
+    if (err) {
+      console.error(err);
+    } else {
+      console.log(`Successfully generated ${fileName}!`);
+    }
+  });
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+  inquirer
+    .prompt(questions)
+    .then((answers) => {
+      // Process the user's answers and generate the README content
+      const readmeContent = `
+# ${answers.title}
+
+## Description
+${answers.description}
+
+## Instructions
+${answers.instructions}
+`;
+
+      // Write the README file
+      writeToFile('README.md', readmeContent);
+    })
+    .catch((error) => {
+      console.error(error);
+    });
+}
 
 // Function call to initialize app
 init();
