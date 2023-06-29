a1102a29f6acf6b39b58eb1dccdd6fa27d3e6acb
Sage Lupi <sagelupi@Sages-MacBook-Pro.local>
2023-06-28 20:51:52 -0400

    Added in more functions

--- Develop/utils/index.js
+++ Develop/utils/index.js
@@ -1,38 +1,63 @@
+// TODO: Create an array of questions for user input
+const inquirer = require('inquirer');
+const questions = [
+    // your questions here
+  ];
+  
+  // TODO: Create a function to write README file
+  function writeToFile(fileName, data) {
+    // your writeToFile implementation
+  }
+  
+  // TODO: Create a function to initialize app
+  async function init() {
+    const { default: inquirer } = await import('inquirer');
+    // rest of your code
+  }
+  
+  // Function call to initialize app
+  init();
+  
+  
+  // Function call to initialize app
+  init();
+  
+
+
 // TODO: Include packages needed for this application 
 // Added in package-lock.json
-
-const inquirer = require('inquirer');
-const fs = require('fs');
+//const inquirer = require('inquirer');
+//const fs = require('fs');
 
 // TODO: Create an array of questions for user input
-const questions = [
-{
-    type: 'input',
-    name: 'title',
-    message: 'What is the title of your project?'
-},
-{
-    type: 'input',
-    name: 'description',
-    message: 'Please provide a brief description of your project.'
-},
-{
-    type: 'input',
-    name: 'instructions',
-    message: 'Please explain how to use your website.'
-}
-];
+//const questions = [
+// {
+    //  type: 'input',
+    //  name: 'title',
+    //  message: 'What is the title of your project?'
+//},
+//{
+    //  type: 'input',
+    //  name: 'description',
+    //  message: 'Please provide a brief description of your project.'
+//},
+//{
+    //  type: 'input',
+    //  name: 'instructions',
+    //  message: 'Please explain how to use your website.'
+//}
+//];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {
-    fs.writeFile(fileName, data, (err) => {
-        if(err) {
-            console.error(err);
-        } else {
-            console.log(`Successfully generated ${filename}!`);
-        }
-    })
-}
+//function writeToFile(fileName, data) {
+    //fs.writeFile(fileName, data, (err) => {
+        //if(err) {
+         //   console.error(err);
+        //} else {
+        //  console.log(`Successfully generated ${filename}!`);
+        //}
+    //})
+//}
 
 // TODO: Create a function to initialize app
 function init() {}
